f787e9bbc450c9d503769896c3a7b41270d4c53e
Yury Staravoitau <MorfinAll@mail.ru>
2018-03-12 15:14:07 +0300

    Implement task with sort but all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if (array.length === 0) {
+    return 0;
+  }
+  array.sort(function (a, b) {
+    return a - b;
+  })
+
+  let counter = 1;
+  let maxCounter = counter;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] !== array[i - 1]) {
+      if (array[i] - array[i - 1] === 1) {
+        counter++;
+      }
+      else {
+        if (counter > maxCounter) {
+          maxCounter = counter;
+        }
+        counter = 1;
+      }
+    }
+  }
+  return maxCounter;
 }
